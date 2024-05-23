21979bcb7ba09b0ef39165c3ccd7a5f945b2f563
huu nhat <nhat200901@gmail.com>
2024-05-24 01:36:13 +0700

    update

--- movie-app/src/pages/user/index.tsx
+++ movie-app/src/pages/user/index.tsx
@@ -1,15 +1,10 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import ListMoviePage from "./home/list-movie/ListMovie";
-import HomeCarousel from "./_components/HomeCarousel/HomeCarousel";
-import Header from "./_components/header/Header";
-import Footer from "./_components/footer/Footer";
-import SystemCinema from "./home/system-cinema/SystemCinema";
 
 export default function IndexHome() {
   return (
     <div>
-       <Outlet />
+      <Outlet />
     </div>
   );
 }
